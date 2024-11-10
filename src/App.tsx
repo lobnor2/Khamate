b3a3a8f1af1cfa74947de3d01bf8a07e96d91c55
lobsang norbu <lnorbu151@gmail.com>
2024-11-10 10:53:34 -0500

    add tailwind and shadcn library

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-  return <>Hello world</>;
+  return (
+    <>
+      <Button>click me</Button>
+    </>
+  );
 }
 
 export default App;
