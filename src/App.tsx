82f3ac926846611d2b122d564244334d5ff33095
lobsang norbu <lnorbu151@gmail.com>
2024-11-10 11:40:49 -0500

    add themeprovider for dark and light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
-import { Button } from "./components/ui/button";
+import Layout from "./components/layout";
+import { ThemeProvider } from "./context/theme-provider";
+import WeatherDashboard from "./pages/weather-dashboard";
+import CityPage from "./pages/city-page";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Button>hello</Button>} />
-        <Button>click me</Button>
-      </Routes>
+      <ThemeProvider defaultTheme="dark">
+        <Layout>
+          <Routes>
+            <Route path="/" element={<WeatherDashboard />} />
+            <Route path="/city/:cityName" element={<CityPage />} />
+          </Routes>
+        </Layout>
+      </ThemeProvider>
     </Router>
   );
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,9 +3,17 @@ import type { PropsWithChildren } from "react";
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <div className="border border-red-500">
-      <header></header>
-      {children}
-      <footer></footer>
+      <header>Headerrrr</header>
+      <main className=" min-h-[100vh] container mx-auto px-4 py-8">
+        {children}
+      </main>
+      <footer className="border-t backdrop-blur py-7">
+        <div className="container mx-auto">
+          <p className="text-gray-500 font-extralight text-center">
+            Made by lobnor
+          </p>
+        </div>
+      </footer>
     </div>
   );
 };
