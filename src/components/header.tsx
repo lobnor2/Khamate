a1fde840493c41e5c78056617ad45af7d8fcfb62
Lobsang Norbu <50455820+lobnor2@users.noreply.github.com>
2024-11-17 23:22:11 -0500

    remove blur background from header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   const isDark = theme === "dark";
 
   return (
-    <header className="sticky top-0 py-3 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 border border-green-500">
+    <header className="sticky top-0 w-full shadow-md bg-background border-b backdrop-blur">
       <div className="container mx-auto flex h-16 items-center justify-between">
         <Link
           to="/"
